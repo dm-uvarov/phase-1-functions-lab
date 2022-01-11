f6a3adbd3bb76c1c57c929a5545487ae20eabfbb
Jimmy <dm.a.uvarov@gmail.com>
2022-01-11 00:51:41 -0500

    commit tree 12175bbdabcbd059ae2da67e88fed0b2f2c4a0b3

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(dist){
+    return Math.abs(dist-42);
+}//end of distanceFromHqInBlocks
+
+function distanceFromHqInFeet(dist){
+    return Math.round(Math.abs(264*(dist-42)));
+}//end of function distanceFromHqInFeet(dist)
+
+function calculatesFarePrice(startpoint,endpoint){
+    let dist = Math.round(Math.abs(264*(endpoint - startpoint)));
+    if (dist < 400) {
+        return 0;
+    } else if (dist < 2000){
+        return (dist-400)*0.02;
+    } else if (dist < 2500 ){
+        return 25;
+    } else {
+        return "cannot travel that far"
+    }
+    
+}// end of function calculatesFarePrice(startpoint,endpoint)
+
+function distanceTravelledInFeet(startpoint,endpoint){
+    return Math.round(Math.abs(264*(endpoint - startpoint)));
+}
